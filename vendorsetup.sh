09210447ae221bef8b485a0fa53e8828cf00b23c
iykequame <zac6ix@gmail.com>
2017-09-25 09:26:17 +0000

    set userdebug as build variant | 25/07/17

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-for var in eng user userdebug; do
+for var in userdebug; do
   add_lunch_combo lineage_Camon_C7-$var
 done
