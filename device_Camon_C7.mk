5a10accaa3bcb0e0379d7446add3b3f2e1879398
iykequame <zac6ix@gmail.com>
2017-09-26 01:05:14 +0000

    Rebasing |prebuilt kernel 3.18.19+| 26/09/17

--- device_Camon_C7.mk
+++ device_Camon_C7.mk
@@ -54,18 +54,14 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/root/fstab.mt6735:root/fstab.mt6735 \
-	$(LOCAL_PATH)/rootdir/root/init.modem.rc:root/init.modem.rc \
-	$(LOCAL_PATH)/rootdir/root/init.mt6735.rc:root/init.mt6735.rc \
-	$(LOCAL_PATH)/rootdir/root/init.mt6735.usb.rc:init.mt6735.usb.rc \
-	$(LOCAL_PATH)/rootdir/root/init.project.rc:root/init.project.rc \
-	$(LOCAL_PATH)/rootdir/root/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
-	$(LOCAL_PATH)/rootdir/root/enableswap.sh:root/enableswap.sh \
-	$(LOCAL_PATH)/rootdir/root/factory_init.project.rc:root/factory_init.project.rc \
-	$(LOCAL_PATH)/rootdir/root/factory_init.rc:root/factory_init.rc \
-	$(LOCAL_PATH)/rootdir/root/fstab.charger:root/fstab.charger \ 
-	$(LOCAL_PATH)/rootdir/root/recovery.fstab:root/recovery.fstab \
-	$(LOCAL_PATH)/rootdir/root/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/root/fstab.mt6735:root/fstab.mt6735 \
+    $(LOCAL_PATH)/rootdir/root/init.mt6735.rc:root/init.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/root/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/root/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/root/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+    $(LOCAL_PATH)/rootdir/root/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/rootdir/root/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/root/init.project.rc:root/init.project.rc \
     $(LOCAL_KERNEL):kernel
 
 # Permissions
